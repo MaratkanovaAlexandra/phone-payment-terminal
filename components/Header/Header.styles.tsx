fa57e16fe96b85d427aea1b74aae132dc755d2a0
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2022-04-22 14:42:18 +0400

    feat: create header component

---
+++ components/Header/Header.styles.tsx
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+import dark from "../../public/burger-dark.svg";
+import light from "../../public/burger-light.svg";
+
+export const StyledHeader = styled.header<{ transparent?: boolean }>`
+  ${({ transparent }) => transparent ? `` : `background-color: #fff;`};
+  margin: 0 auto;
+  padding: 30px 20px;
+`;
+
+export const HeaderConteiner = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Logo = styled.figure<{ transparent?: boolean }>`
+  ${({ transparent }) => transparent ? `color: #fff;` : `color: #d15eff;`};
+  font-size: 32px;
+`;
+
+export const Burger = styled.figure<{ transparent?: boolean; open: boolean }>`
+  position: relative;
+  z-index: 2;
+  margin-left: auto;
+  width: 50px;
+  aspect-ratio: 1;
+  background-image: ${({ transparent }) => transparent ? `url(${light.src})` : `url(${dark.src})`};
+  transition: transform 0.5s ease-in-out;
+  transform: ${({ open }) => open ? "rotate(90deg)" : "rotate(0)"};
+
+  @media (min-width: 500px) {
+    display: none;
+  }
+`;
+
+export const Shadow = styled.div<{open: boolean}>`
+  display: ${({ open }) => open ? "block" : "none"};
+  position: absolute;
+  inset: 0 0 auto auto;
+  height: 100vh;
+  width: 100vw;
+  background-color: #4f4f4f2f;
+`;
+
+export const Nav = styled.nav<{ transparent?: boolean, open: boolean }>`
+  position: absolute;
+  inset:${({ open }) => open ? "0 0 auto auto" : "0 -100% auto auto"};
+  background-color: ${({ transparent }) => transparent ? "#522265ce" : "#fff"};
+  height: 100vh;
+  width: 320px;
+  padding-top: 170px;
+  z-index: 1;
+  transition: all 0.5s ease-in-out;
+
+  & li {
+    ${({ transparent }) => (transparent ? `color: #fff;` : `color: #222;`)};
+    cursor: pointer;
+    font-size: 24px;
+    font-weight: 400;
+
+    &[data-disabled="true"] {
+      padding: 0 0 3px 0;
+      border-bottom: 3px solid #d15eff;
+    }
+  }
+
+  & > ul {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
+
+    list-style: none;
+  }
+
+  @media (min-width: 500px) {
+    margin-left: auto;
+    position: static;
+    background-color: transparent;
+    height: auto;
+    width: auto;
+    transform: none;
+    transition: none;
+    padding: 0;
+
+    & > ul {
+      flex-direction: row;
+    }
+  }
+`;
---
+++ components/Header/Header.tsx
@@ -0,0 +1,46 @@
+import { useRouter } from "next/router";
+import { FC, useState } from "react";
+import Link from "next/link";
+import Wrapper from "../Wrapper";
+
+import { StyledHeader, HeaderConteiner, Shadow, Burger, Logo, Nav } from "./Header.styles";
+
+type HeaderProps = {
+  transparent?: boolean;
+};
+
+const Header: FC<HeaderProps> = (props) => {
+  const { transparent } = props;
+  const route = useRouter().route;
+  const [ open, setOpen ] = useState(false);
+
+  return (
+      <StyledHeader transparent={transparent}>
+        <Shadow open={open} onClick={() => setOpen(false)}/>
+        <Wrapper>
+          <HeaderConteiner> 
+            <Logo transparent={transparent}>
+              <h1>PPT</h1>
+            </Logo>
+            <Burger transparent={transparent} open={open} onClick={() => setOpen(!open)} />
+            <Nav transparent={transparent} open={open}>
+              <ul>
+                <li data-disabled = {route === "/"}>
+                  <Link href="/">Welcome</Link>
+                </li>
+                <li data-disabled = {route === "/providers"}>
+                  <Link href="/providers">Providers</Link>
+                </li>
+              </ul>
+            </Nav>
+          </HeaderConteiner>
+        </Wrapper>
+      </StyledHeader>
+  );
+};
+
+Header.defaultProps = {
+  transparent: false,
+};
+
+export default Header;
