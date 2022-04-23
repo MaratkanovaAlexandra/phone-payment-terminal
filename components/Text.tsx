016de100c253853542940fda1eaeb5be29ab5cf7
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2022-04-23 09:55:40 +0400

    feat: create text component

---
+++ components/Text.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+type TextProps = {
+  margin?: string;
+  width?: string;
+  center?: boolean;
+  color?: string;
+};
+
+const Text = styled.p<TextProps>`
+  font-size: 16px;
+  font-weight: 400;
+  
+  color: ${({ color }) => color || "#000"};
+  margin: ${({ margin }) => margin || "0"};
+  width: ${({ width }) => width || "auto"};
+  ${(center) => (center ? "text-align: center;" : "")}
+`;
+
+export default Text;
