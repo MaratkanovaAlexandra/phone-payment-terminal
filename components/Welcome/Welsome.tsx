3ab668b718e41ee3de116d28c21d49e3464ae296
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2022-04-28 20:06:51 +0400

    refactor: remove <main>

--- components/Welcome/Welsome.tsx
+++ components/Welcome/Welsome.tsx
@@ -15,28 +15,26 @@ const Welcome: FC = () => {
   const router = useRouter();
 
   return (
-    <main>
-      <Background image={Back.src} size="cover">
-        <Header transparent />
-        <Wrapper padding="110px 10px 354px">
-          <Center direction="column">
-            <Title fontSize="large" margin="0 0 20px" color="#fff">
-              Welcome
-            </Title>
-            <Text margin="0 0 5px" center color="#fff">
-              This is my test task using next.js.
-            </Text>
-            <Text margin="0 0 40px" center color="#fff">
-              Unfortunately, I'm not very good at design, but I hope you'll like
-              it.
-            </Text>
-            <Button transparent clickHandler={() => router.push("/providers")}>
-              Let's get started!
-            </Button>
-          </Center>
-        </Wrapper>
-      </Background>
-    </main>
+    <Background image={Back.src} size="cover">
+      <Header transparent />
+      <Wrapper padding="110px 10px 354px">
+        <Center direction="column">
+          <Title fontSize="large" margin="0 0 20px" color="#fff">
+            Welcome
+          </Title>
+          <Text margin="0 0 5px" center color="#fff">
+            This is my test task using next.js.
+          </Text>
+          <Text margin="0 0 40px" center color="#fff">
+            Unfortunately, I'm not very good at design, but I hope you'll like
+            it.
+          </Text>
+          <Button transparent clickHandler={() => router.push("/providers")}>
+            Let's get started!
+          </Button>
+        </Center>
+      </Wrapper>
+    </Background>
   );
 };
 
