35197cd68dd5f57f5a8e8c47ed8a56b1094b1c34
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2022-05-02 17:17:07 +0400

    feat: create form usinf formik

--- components/AddProvider/AddProvider.style.tsx
+++ components/AddProvider/AddProvider.style.tsx
@@ -2,37 +2,74 @@ import styled from "styled-components";
 
 export const Background = styled.div`
   min-height: 86vh;
-  background-color: #f1f1f1;
-  padding: 50px 0 0;
+  background-color: #fafafa;
+  padding: 0;
+
+  @media (min-width: 500px) {
+    padding: 50px 0 0;
+    background-color: #f1f1f1;
+  }
 `;
 
 export const Container = styled.div`
   background-color: #fafafa;
   border-radius: 5px;
-  box-shadow: 0px 10px 8px 0px rgba(66, 52, 79, 0.2);
-  padding: 40px 20px 20px;
-`;
+  padding: 20px 20px;
+  height: 83vh;
 
-export const StyledForm = styled.form`
-  margin: 50px 0 0;
-  padding: 15px;
-`;
+  & h2 {
+    text-align: center;
+  }
 
-export const FormControl = styled.div`
-  font-size: 24px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+  & form {
+    margin: 10px 0 0;
+    padding: 5px;
 
-  margin-bottom: 30px;
+    & button {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+    }
+  }
 
-  & input {
-    height: 50px;
-    font-size: 20px;
-    padding: 0 10px;
+  @media (min-width: 500px) {
+    padding: 40px 20px 20px;
 
-    &:focus {
-      outline: none;
+    height: min-content;
+    background-color: #fafafa;
+    box-shadow: 0px 10px 8px 0px rgba(66, 52, 79, 0.2);
+    & h2 {
+      text-align: left;
+    }
+
+    & form {
+      margin: 50px 0 0;
+      padding: 15px;
+
+      & button {
+        width: auto;
+        display: inline-block;
+      }
     }
   }
 `;
+
+// export const StyledForm = styled.form`
+//   margin: 10px 0 0;
+//   padding: 5px;
+
+//   & button {
+//     width: 100%;
+//     display: flex;
+//     justify-content: center;
+//   }
+
+//   @media (min-width: 500px) {
+//     margin: 50px 0 0;
+//     padding: 15px;
+//     & button {
+//       width: auto;
+//       display: inline-block;
+//     }
+//   }
+// `;
--- components/AddProvider/AddProvider.tsx
+++ components/AddProvider/AddProvider.tsx
@@ -1,23 +1,85 @@
+import { useRouter } from "next/router";
 import { FC } from "react";
+import Image from "next/image";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
 
 import Wrapper from "../Wrapper";
 import Header from "../Header";
-import Form from "./Form";
+import Button from "../Button";
 import Title from "../Title";
 import { Background, Container } from "./AddProvider.style";
+import Input from "../Input";
 
-const AddProvider: FC = () => (
-  <>
-    <Header />
-    <Background>
-      <Wrapper maxWidth="700px">
-        <Container>
-          <Title fontSize="normal">Create a new provider</Title>
-          <Form />
-        </Container>
-      </Wrapper>
-    </Background>
-  </>
-);
+const validate = Yup.object({
+  name: Yup.string()
+    .max(15, "Must be 15 characters or less")
+    .min(2, "Password must be at least 2 charaters")
+    .required("Required"),
+  icon: Yup.string().required("Required"),
+});
+
+const AddProvider: FC = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <Header />
+      <Background>
+        <Wrapper maxWidth="700px">
+          <Container>
+            <Formik
+              initialValues={{
+                name: "",
+                icon: "",
+              }}
+              validationSchema={validate}
+              onSubmit={(values) => {
+                if (!values.icon || !values.name) return;
+                const provider = {
+                  id: 0,
+                  ...values,
+                };
+                fetch("http://localhost:5050/providers/create", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(provider),
+                });
+                router.push("/providers");
+              }}
+            >
+              {(formik) => (
+                <div>
+                  <Title fontSize="normal">Create a new provider</Title>
+                  <Form>
+                    <Input label="Provider Name:" name="name" type="text" />
+                    <Input label="Icon:" name="icon" type="text" />
+                    <Button
+                      type="submit"
+                      disabled={
+                        !(formik.values.name.length > 1) || !formik.values.icon
+                      }
+                    >
+                      <Image
+                        src="/plus.svg"
+                        alt="plus"
+                        height="48px"
+                        width="48px"
+                        layout="fixed"
+                      />
+                      Add Provider
+                    </Button>
+                  </Form>
+                </div>
+              )}
+            </Formik>
+          </Container>
+        </Wrapper>
+      </Background>
+    </>
+  );
+};
 
 export default AddProvider;
