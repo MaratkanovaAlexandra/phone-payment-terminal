ff46665d8418dbc9f044e529eff1fd005e971d7f
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2022-05-06 16:59:04 +0400

    feat: mask input

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,16 +1,23 @@
 import { FC } from "react";
 import { ErrorMessage, useField } from "formik";
+import InputMask from "react-input-mask";
 import FormControl from "./Input.style";
 
 type InputProps = any;
 
-const Input: FC<InputProps> = ({ label, ...props }) => {
+const Input: FC<InputProps> = ({ label, mask, ...props }) => {
   const [field, meta] = useField(props as any);
 
   return (
     <FormControl valid={!(meta.touched && !!meta.error)}>
       <label htmlFor={field.name}>{label}</label>
-      <input {...field} {...props} autoComplete="off" />
+      <InputMask
+        mask={mask}
+        maskChar=" "
+        {...field}
+        {...props}
+        autoComplete="off"
+      />
       <ErrorMessage component="div" name={field.name} className="error" />
     </FormControl>
   );
