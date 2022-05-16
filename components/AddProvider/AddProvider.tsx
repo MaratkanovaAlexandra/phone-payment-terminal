6260a74bf5574f374e6dc1eed5d35836729406e0
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2022-05-16 19:42:22 +0400

    fix: separate logic

--- components/AddProvider/AddProvider.tsx
+++ components/AddProvider/AddProvider.tsx
@@ -4,14 +4,13 @@ import Image from "next/image";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 
-import Wrapper from "../Wrapper";
-import Header from "../Header";
-import Button from "../Button";
-import Title from "../Title";
-import Banner from "../Banner";
-import Loader from "../Loader";
+import { Wrapper } from "../Wrapper";
+import { Header } from "../Header";
+import { Button } from "../Button";
+import { Title } from "../Title";
+import { Loader } from "../Loader";
 import { Background, Container } from "./AddProvider.style";
-import Input from "../Input";
+import { Input } from "../Input";
 
 const validate = Yup.object({
   name: Yup.string()
@@ -21,90 +20,70 @@ const validate = Yup.object({
   icon: Yup.string().required("Required"),
 });
 
-const AddProvider: FC = () => {
+const validateButton = (values: { name: string; icon: string }) =>
+  !(values.name.length > 1) || !values.icon;
+
+export const AddProviderPage: FC = () => {
   const router = useRouter();
-  const [showBanner, setShowBanner] = useState(false);
-  const [ok, setOk] = useState(true);
-  const [code, setCode] = useState(0);
-  const [bannerMessage, setBannerMessage] = useState("");
   const [loadings, setLoadings] = useState(false);
 
-  return (
-    <>
-      {showBanner && <Banner ok={!ok} code={code} message={bannerMessage} />}
-      <main>
-        <Header />
-        <Background>
-          <Wrapper maxWidth="700px">
-            {loadings && <Loader />}
-            {!loadings && (
-              <Container>
-                <Formik
-                  initialValues={{
-                    name: "",
-                    icon: "",
-                  }}
-                  validationSchema={validate}
-                  onSubmit={async (values) => {
-                    if (!values.icon || !values.name) return;
-                    const provider = {
-                      id: 0,
-                      ...values,
-                    };
-                    setLoadings(true);
-                    const res = await fetch(
-                      "https://tranquil-shelf-20388.herokuapp.com/providers/create",
-                      {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify(provider),
-                      }
-                    );
-                    const { message } = await res.json();
-                    setLoadings(false);
-                    setShowBanner(true);
-                    setOk(res.ok);
-                    setCode(res.status);
-                    setBannerMessage(message);
+  const submit = async (values: { name: string; icon: string }) => {
+    if (!values.icon || !values.name) return;
+    const provider = {
+      id: 0,
+      ...values,
+    };
+    setLoadings(true);
+    await fetch("https://tranquil-shelf-20388.herokuapp.com/providers/create", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(provider),
+    });
+    setLoadings(false);
+    router.push("/providers");
+  };
 
-                    if (res.ok) {
-                      setTimeout(() => router.push("/providers"), 500);
-                    }
-                  }}
-                >
-                  {(formik) => (
-                    <div>
-                      <Title fontSize="normal">Create a new provider</Title>
-                      <Form>
-                        <Input label="Provider Name:" name="name" type="text" />
-                        <Input label="Icon:" name="icon" type="text" />
-                        <Button
-                          type="submit"
-                          disabled={
-                            !(formik.values.name.length > 1) ||
-                            !formik.values.icon
-                          }
-                        >
-                          <Image
-                            src="/plus.svg"
-                            alt="plus"
-                            height="48px"
-                            width="48px"
-                            layout="fixed"
-                          />
-                          Add Provider
-                        </Button>
-                      </Form>
-                    </div>
-                  )}
-                </Formik>
-              </Container>
-            )}
-          </Wrapper>
-        </Background>
-      </main>
-    </>
+  return (
+    <main>
+      <Header />
+      <Background>
+        <Wrapper maxWidth="700px">
+          {loadings ? (
+            <Loader />
+          ) : (
+            <Container>
+              <Formik
+                initialValues={{ name: "", icon: "" }}
+                validationSchema={validate}
+                onSubmit={submit}
+              >
+                {(formik) => (
+                  <>
+                    <Title fontSize="normal">Create a new provider</Title>
+                    <Form>
+                      <Input label="Provider Name:" name="name" type="text" />
+                      <Input label="Icon:" name="icon" type="text" />
+                      <Button
+                        type="submit"
+                        disabled={validateButton(formik.values)}
+                      >
+                        <Image
+                          src="/plus.svg"
+                          alt="plus"
+                          height="48px"
+                          width="48px"
+                          layout="fixed"
+                        />
+                        Add Provider
+                      </Button>
+                    </Form>
+                  </>
+                )}
+              </Formik>
+            </Container>
+          )}
+        </Wrapper>
+      </Background>
+    </main>
   );
 };
-
-export default AddProvider;
--- components/Provider/Provider.tsx
+++ components/Provider/Provider.tsx
@@ -2,15 +2,15 @@ import { FC, useState } from "react";
 import { useRouter } from "next/router";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
-import ProviderProps from "../../interfaces/Provider";
+import { IProvider } from "../../interfaces/Provider";
 
-import Header from "../Header";
-import Wrapper from "../Wrapper";
-import Button from "../Button";
-import Title from "../Title";
-import Input from "../Input";
-import Banner from "../Banner";
-import Loader from "../Loader";
+import { Header } from "../Header";
+import { Wrapper } from "../Wrapper";
+import { Button } from "../Button";
+import { Title } from "../Title";
+import { Input } from "../Input";
+import { Banner } from "../Banner";
+import { Loader } from "../Loader";
 import { Background, Container } from "./Provider.style";
 
 const regex = /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/;
@@ -18,21 +18,19 @@ const regex = /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/;
 const validate = Yup.object({
   phoneNumber: Yup.string()
     .matches(regex, "Must be a phone number")
+    .min(16, "Must be a phone number")
     .required("Required"),
   amount: Yup.string()
-    .test("not-empty", "Can not be empty", (val) =>
-      val ? !!(val as string).split(" ")[0] : false
-    )
     .test("amount", "Must be more than 0 and less than 1000", (val) =>
       val
-        ? +(val as string).split(" ")[0] >= 1 &&
-          +(val as string).split(" ")[0] <= 1000
+        ? +(val as string).split("rub")[0] >= 1 &&
+          +(val as string).split("rub")[0] <= 1000
         : false
     )
     .required("Required"),
 });
 
-const Provider: FC<ProviderProps> = (props) => {
+export const ProviderPage: FC<IProvider> = (props) => {
   const { id, name, icon } = props;
   const router = useRouter();
   const [showBanner, setShowBanner] = useState(false);
@@ -41,6 +39,34 @@ const Provider: FC<ProviderProps> = (props) => {
   const [bannerMessage, setBannerMessage] = useState("");
   const [loadings, setLoadings] = useState(false);
 
+  const submit = async (values: { phoneNumber: string; amount: string }) => {
+    setLoadings(true);
+    const res = await fetch(
+      `https://tranquil-shelf-20388.herokuapp.com/pay/${id}`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(values),
+      }
+    );
+    const { message } = await res.json();
+    setLoadings(false);
+    setShowBanner(true);
+    setOk(res.ok);
+    setCode(res.status);
+    setBannerMessage(message);
+
+    if (res.ok) {
+      setTimeout(() => router.push("/providers"), 500);
+    }
+  };
+
+  const validateButton = (values: { phoneNumber: string; amount: string }) =>
+    +values.amount.split("rub")[0] <= 0 ||
+    +values.amount.split("rub")[0] > 1000 ||
+    !regex.test(values.phoneNumber) ||
+    values.phoneNumber.length < 16;
+
   return (
     <>
       {showBanner && <Banner ok={ok} code={code} message={bannerMessage} />}
@@ -48,39 +74,17 @@ const Provider: FC<ProviderProps> = (props) => {
         <Header />
         <Background>
           <Wrapper maxWidth="700px">
-            {loadings && <Loader />}
-            {!loadings && (
+            {loadings ? (
+              <Loader />
+            ) : (
               <Container>
                 <Formik
-                  initialValues={{
-                    phoneNumber: "",
-                    amount: "",
-                  }}
+                  initialValues={{ phoneNumber: "", amount: "0" }}
                   validationSchema={validate}
-                  onSubmit={async (values) => {
-                    setLoadings(true);
-                    const res = await fetch(
-                      `https://tranquil-shelf-20388.herokuapp.com/pay/${id}`,
-                      {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify(values),
-                      }
-                    );
-                    const { message } = await res.json();
-                    setLoadings(false);
-                    setShowBanner(true);
-                    setOk(res.ok);
-                    setCode(res.status);
-                    setBannerMessage(message);
-
-                    if (res.ok) {
-                      setTimeout(() => router.push("/providers"), 500);
-                    }
-                  }}
+                  onSubmit={submit}
                 >
                   {(formik) => (
-                    <div>
+                    <>
                       <Title fontSize="normal">
                         <img src={icon} alt={name} width="30" height="30" />
                         {name}
@@ -92,24 +96,15 @@ const Provider: FC<ProviderProps> = (props) => {
                           type="text"
                           mask="+7-999-999-99-99"
                         />
-                        <Input
-                          label="Amount:"
-                          name="amount"
-                          type="text"
-                          mask="9999 rub."
-                        />
+                        <Input label="Amount:" name="amount" type="number" />
                         <Button
                           type="submit"
-                          disabled={
-                            +formik.values.amount.split(" ")[0] <= 0 ||
-                            +formik.values.amount.split(" ")[0] > 1000 ||
-                            !regex.test(formik.values.phoneNumber)
-                          }
+                          disabled={validateButton(formik.values)}
                         >
                           Send
                         </Button>
                       </Form>
-                    </div>
+                    </>
                   )}
                 </Formik>
               </Container>
@@ -120,5 +115,3 @@ const Provider: FC<ProviderProps> = (props) => {
     </>
   );
 };
-
-export default Provider;
